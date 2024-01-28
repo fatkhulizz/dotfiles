11f764040f37bdf40c2aa858db1868ed7a1e3e24
fatkhulizz <fatkhul7izza@gmail.com>
2024-01-28 22:07:38 +0700

    refactor: move plugins with simple config to single file

--- nvim/.config/nvim/lua/custom/plugins/colorizer.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "NvChad/nvim-colorizer.lua",
-  lazy = true,
-  cmd = "ColorizerToggle",
-  config = function()
-    require("colorizer").setup({})
-  end,
-}
--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -1,5 +1,74 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    Lazy = true,
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    keys = { { "<leader>rp", function() require("spectre").open() end, desc = "[R]e[P]lace in files (Spectre)" }, },
+  },
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    lazy = true,
+    cmd = "ColorizerToggle",
+    config = function()
+      require("colorizer").setup({})
+    end,
+  },
+
+  {
+    "j-hui/fidget.nvim",
+    event = "BufEnter",
+    opts = { notification = { window = { winblend = 0, }, }, }
+  },
+
+  {
+    -- git plugin
+    'tpope/vim-fugitive',
+    lazy = true,
+    cmd = 'Git',
+    keys = {
+      { '<leader>gg', '<CMD>Git<CR>',        desc = 'Git status' },
+      { '<leader>gc', '<CMD>Git commit<CR>', desc = 'Git commit' },
+    },
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  {
+    "folke/zen-mode.nvim",
+    Lazy = true,
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        options = {
+          laststatus = 0,
+        },
+        tmux = { enabled = true },
+        wezterm = { enabled = true },
+      },
+    },
+    keys = { { '<leader>z', '<CMD>ZenMode<CR>', desc = 'toggle [Z]en Mode' }, },
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  }
+}
