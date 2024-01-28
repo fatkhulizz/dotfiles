b1863e05d73dc04c29dfe6b81ea5619985d9ae2f
fatkhulizz <fatkhul7izza@gmail.com>
2024-01-28 22:09:43 +0700

    feat: add git status on oil and better keymaps

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -1,10 +1,8 @@
 return {
   {
     "stevearc/oil.nvim",
-    -- enabled = false,
-    -- lazy = true,
-    -- event = "BufEnter",
     opts = {
+      win_options = { signcolumn = "yes:2" },
       columns = {
         "icon",
         -- "permissions",
@@ -12,24 +10,9 @@ return {
         "mtime",
       },
       keymaps = {
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
         ["<A-l>"] = "actions.select",
-        ["<C-s>"] = "actions.select_vsplit",
-        ["<C-h>"] = "actions.select_split",
-        ["<C-t>"] = "actions.select_tab",
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["q"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
         ["<A-h>"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = "actions.tcd",
-        ["gs"] = "actions.change_sort",
-        ["gx"] = "actions.open_external",
-        ["g."] = "actions.toggle_hidden",
+        ["q"] = "actions.close",
       },
     },
     -- Optional dependencies
@@ -38,6 +21,14 @@ return {
   {
     vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent dir" }),
     vim.keymap.set("n", "<A-h>", "<CMD>Oil<CR>", { desc = "Open parent dir", noremap = true }),
-    vim.keymap.set("n", "<leader>h", "<CMD>Oil .<CR>", { desc = "Open root dir" }),
+    vim.keymap.set("n", "<leader>hh", "<CMD>Oil .<CR>", { desc = "Open root dir" }),
+  },
+
+  -- remove this plugin if it already implemented on oil.nvim
+  -- https://github.com/stevearc/oil.nvim/issues/85
+  {
+    "refractalize/oil-git-status.nvim",
+    dependencies = { "stevearc/oil.nvim", },
+    config = true,
   },
 }
