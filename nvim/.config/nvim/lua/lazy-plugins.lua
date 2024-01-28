9000d73fd0e648e41da5423d8e93d244a7bf88a6
fatkhulizz <fatkhul7izza@gmail.com>
2024-01-28 22:13:04 +0700

    feat: disable some builtin plugins

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -1,86 +1,95 @@
 -- [[ Configure plugins ]]
 require('lazy').setup({
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        'gzip',
-        -- "matchit",
-        -- "matchparen",
-        'netrwPlugin',
-        'tarPlugin',
-        'tohtml',
-        'tutor',
-        'zipPlugin',
+    'tpope/vim-sleuth',
+    {
+      -- LSP Configuration & Plugins
+      'neovim/nvim-lspconfig',
+      event = { 'BufReadPre', 'BufNewFile' },
+      dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        { 'williamboman/mason.nvim', config = true },
+        'williamboman/mason-lspconfig.nvim',
+
+        -- Additional lua configuration, makes nvim stuff amazing!
+        { 'folke/neodev.nvim',       opts = {} },
       },
     },
-  },
-  'tpope/vim-sleuth',
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
 
-      -- Additional lua configuration, makes nvim stuff amazing!
-      { "folke/neodev.nvim",       opts = {} },
+    {
+      -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      event = 'InsertEnter',
+      dependencies = {
+        -- Snippet Engine & its associated nvim-cmp source
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
 
-    },
-  },
+        -- Adds LSP completion capabilities
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
 
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = "InsertEnter",
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
-
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  event = "VeryLazy", opts = {} },
-  { 'numToStr/Comment.nvim', event = "VeryLazy", opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
+        -- Adds a number of user-friendly snippets
+        'rafamadriz/friendly-snippets',
       },
     },
-  },
 
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
+    -- Useful plugin to show you pending keybinds.
+    { 'folke/which-key.nvim',  event = 'VeryLazy', opts = {} },
+    { 'numToStr/Comment.nvim', event = 'VeryLazy', opts = {} },
+
+    -- Fuzzy Finder (files, lsp, etc)
+    {
+      'nvim-telescope/telescope.nvim',
+      branch = '0.1.x',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+          'nvim-telescope/telescope-fzf-native.nvim',
+          build = 'make',
+          cond = function()
+            return vim.fn.executable 'make' == 1
+          end,
+        },
+      },
     },
-    build = ':TSUpdate',
-  },
 
-  require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-  { import = 'custom.plugins' },
-}, {})
+    {
+      -- Highlight, edit, and navigate code
+      'nvim-treesitter/nvim-treesitter',
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+      build = ':TSUpdate',
+    },
+    require 'kickstart.plugins.autoformat',
+    -- require 'kickstart.plugins.debug',
+    { import = 'custom.plugins' },
+  },
+  {
+    performance = {
+      cache = {
+        enabled = true,
+      },
+      reset_packpath = true, -- reset the package path to improve startup time
+      rtp = {
+        reset = true,        -- reset the runtime path to $VIMRUNTIME and your config directory
+        ---@type string[]
+        paths = {},          -- add any custom paths here that you want to includes in the rtp
+        ---@type string[] list any plugins you want to disable here
+        disabled_plugins = {
+          "gzip",
+          -- "matchit",
+          -- "matchparen",
+          "Intro",
+          "netrwPlugin",
+          "tarPlugin",
+          "tohtml",
+          "tutor",
+          "zipPlugin",
+        },
+      },
+    },
+  }
+)
 
 -- vim: ts=2 sts=2 sw=2 et
