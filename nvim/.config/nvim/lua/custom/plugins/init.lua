c7f3ddea2a806ca7ac09a8130c5928e2fa329f79
fatkhulizz <fatkhul7izza@gmail.com>
2024-02-03 19:41:45 +0700

    fix: remove redefinition undo plugin

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -75,22 +75,5 @@ return {
   {
     'stevearc/dressing.nvim',
     opts = {},
-  },
-
-  {
-    "debugloop/telescope-undo.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-      },
-    },
-    keys = { { "<leader>u", "<cmd>Telescope undo<cr>", desc = "[U]ndo history", },
-    },
-    opts = { extensions = { undo = {}, }, },
-    config = function(_, opts)
-      require("telescope").setup(opts)
-      require("telescope").load_extension("undo")
-    end,
   }
 }
