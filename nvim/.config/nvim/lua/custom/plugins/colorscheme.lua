f8e4d154e0c7baaad3575aea66a9547a82d5bc93
fatkhulizz <fatkhul7izza@gmail.com>
2024-02-03 19:36:29 +0700

    feat: add everforest color

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,27 +1,51 @@
+local function neovide_transparent()
+  if vim.g.neovide then
+    return 0
+  else
+    return 1
+  end
+end
+
 return {
+  {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('bamboo').setup {
+        -- optional configuration here
+        -- transparent = neovide_transparent()
+      }
+      -- require('bamboo').load()
+    end,
+  },
+
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = 'hard'
+      vim.g.everforest_enable_italic = 1
+      vim.g.everforest_cursor = 'green'
+      vim.g.everforest_transparent_background = neovide_transparent()
+      vim.g.everforest_float_style = 'dim'
+      vim.g.everforest_better_performance = 0 --default
+
+      vim.cmd.colorscheme('everforest')
+    end,
+  },
+
   -- {
-  --   'ribru17/bamboo.nvim',
+  --   "folke/tokyonight.nvim",
   --   Lazy = false,
   --   priority = 1000,
   --   config = function()
-  --     require('bamboo').setup {
-  --       -- optional configuration here
-  --       toggle_style_key = "<leader>ts",
+  --     require('tokyonight').setup {
+  --       -- style = "moon",
   --       transparent = true,
   --     }
-  --     require('bamboo').load()
+  --     vim.cmd("colorscheme tokyonight")
   --   end,
   -- },
-  {
-    "folke/tokyonight.nvim",
-    Lazy = false,
-    priority = 1000,
-    config = function()
-      require('tokyonight').setup {
-        -- style = "moon",
-        transparent = true,
-      }
-      vim.cmd("colorscheme tokyonight")
-    end,
-  },
 }
