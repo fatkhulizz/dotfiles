2e687ed108e976f5e542f65dc2d63a6868c0996f
fatkhulizz <fatkhul7izza@gmail.com>
2024-03-03 10:10:58 +0700

    fix(nvim): fix ibl line

--- nvim/.config/nvim/lua/custom/plugins/ibl.lua
+++ nvim/.config/nvim/lua/custom/plugins/ibl.lua
@@ -7,10 +7,11 @@ return {
   event = 'VeryLazy',
   opts = {
     indent = {
-      char = '▏',
-      tab_char = '│',
+      -- char = '▎',
+      tab_char = '▎',
     },
-    scope = { enabled = true, show_start = true },
+    -- scope = { enabled = true, show_start = true },
+    scope = {  show_end = true, },
     exclude = {
       filetypes = {
         'help',
